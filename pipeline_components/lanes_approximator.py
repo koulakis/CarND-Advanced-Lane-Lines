b961fc22e7f59694cd09c1dc18504c3ed71ef676
mariosk <marios.koulakis@gmail.com>
2019-02-24 17:52:16 +0100

    Adds lanes detection in when previous polynomial approximation exists.

--- pipeline_components/lanes_approximator.py
+++ pipeline_components/lanes_approximator.py
@@ -84,6 +84,19 @@ class LanePixelsFinder:
         return leftx, lefty, rightx, righty, boxes
 
 
+class LanePixelsFinderFromPolynomial:
+    @staticmethod
+    def find_pixels(image, left_fit, right_fit, margin):
+        nonzero = image.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+
+        left_lane_inds = (np.abs(nonzerox - np.polyval(left_fit, nonzeroy)) < margin).nonzero()
+        right_lane_inds = (np.abs(nonzerox - np.polyval(right_fit, nonzeroy)) < margin).nonzero()
+
+        return nonzerox[left_lane_inds], nonzeroy[left_lane_inds], nonzerox[right_lane_inds], nonzeroy[right_lane_inds]
+
+
 class LaneApproximator(BaseEstimator):
     def __init__(self,  nwindows=9, margin=100, minpix=50, plot_approximation=False):
         self.nwindows = nwindows
@@ -125,20 +138,57 @@ class LaneApproximator(BaseEstimator):
         plt.imshow(out_img)
         plt.show()
 
+    @staticmethod
+    def plot_around_polynomial_curve(image, curve_info, polynomial_info, margin):
+        leftx, lefty, rightx, righty = curve_info
+        left_fitx, right_fitx, ploty = polynomial_info
+
+        out_img = np.dstack((image, image, image))
+        out_img[lefty, leftx] = [255, 0, 0]
+        out_img[righty, rightx] = [0, 0, 255]
+
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin,
+                                                                        ploty])))])
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin,
+                                                                         ploty])))])
+        right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+        window_img = np.zeros_like(out_img)
+        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+
+        plt.figure(figsize=(10, 10))
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.imshow(result)
+
     def fit(self):
         return self
 
-    def transform(self, image):
-        boxes_info = LanePixelsFinder.find_pixels(image, self.nwindows, self.margin, self.minpix)
+    def transform(self, image, pipeline_state):
+        left_fit_state, right_fit_state = [pipeline_state[key] for key in ['left_fit', 'right_fit']]
 
-        leftx, lefty, rightx, righty, boxes = boxes_info
+        boxes_info = (
+            LanePixelsFinder.find_pixels(image, self.nwindows, self.margin, self.minpix)
+            if left_fit_state is None
+            else LanePixelsFinderFromPolynomial.find_pixels(image, left_fit_state, right_fit_state, self.margin))
+
+        leftx, lefty, rightx, righty = boxes_info[:4]
         left_fit, right_fit = LaneApproximator.__fit_polynomials(leftx, lefty, rightx, righty)
 
         ploty = np.linspace(0, image.shape[0] - 1, image.shape[0])
         left_fitx = np.polyval(left_fit, ploty)
         right_fitx = np.polyval(right_fit, ploty)
+        polynomial_info = left_fitx, right_fitx, ploty
 
         if self.plot_approximation:
-            LaneApproximator.plot_boxes_and_fitted_polynomials(image, boxes_info, [left_fitx, right_fitx, ploty])
+            if left_fit_state is None:
+                LaneApproximator.plot_boxes_and_fitted_polynomials(image, boxes_info, polynomial_info)
+            else:
+                LaneApproximator.plot_around_polynomial_curve(image, boxes_info, polynomial_info, self.margin)
 
-        return left_fitx, right_fitx, ploty
+        return left_fitx, right_fitx, ploty, left_fit, right_fit
